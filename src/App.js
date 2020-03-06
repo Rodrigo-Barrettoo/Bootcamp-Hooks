e1842f73426a728ec0946d9739aa9814b3c7712b
Rodrigo-Barrettoo <rodrigonarrettoo@outlook.com>
2020-03-06 12:00:53 -0300

    funções do Hooks

--- src/App.js
+++ src/App.js
@@ -1,7 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect, useMemo, useCallback } from 'react';
 
 function App() {
-  return <h1>Hello world</h1>;
+  const [tech, setTech] = useState([]);
+  const [newTech, setNewTech] = useState('');
+
+  const handleAdd = useCallback(() => {
+    setTech([...tech, newTech]);
+    setNewTech('');
+  }, [newTech, tech]);
+
+  useEffect(() => {
+    const storageTech = localStorage.getItem('tech');
+
+    if (storageTech) {
+      setTech(JSON.parse(storageTech));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('tech', JSON.stringify(tech));
+  }, [tech]);
+
+  /**
+   * Muito utilazado em Calculos que usam apenas algumas informações.
+   */
+  const techSize = useMemo(() => tech.length, [tech]);
+
+  return (
+    <>
+      <ul>
+        {tech.map(t => (
+          <li key={t}> {t} </li>
+        ))}
+      </ul>
+      <strong>Você tem {techSize} tecnologias</strong>
+      <br /> <br />
+      {/**
+       * e. => captura a alteração no input
+       */}
+      <input value={newTech} onChange={e => setNewTech(e.target.value)} />
+      <button type="button" onClick={handleAdd}>
+        Adicionar
+      </button>
+    </>
+  );
 }
 
 export default App;
